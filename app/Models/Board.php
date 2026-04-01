9906c8756628518fe40730d39a9cdc677e691e5c
devarya00 <bitencourtn412@gmail.com>
2026-04-01 14:33:17 -0400

    first commit

---
+++ app/Models/Board.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Hodel;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'user_id'];
+
+    public function columns() 
+    {
+        return $this->hasMany(Column::class)->orderBy('order');
+    }
+}
